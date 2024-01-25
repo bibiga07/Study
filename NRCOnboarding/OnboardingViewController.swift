9e48bcb252d94bf088bded44a2ed8e2e127e29af
Kim ju hwan <minecraft_bos@naver.com>
2024-01-26 00:44:09 +0900

    NRCOnboarding_1!

---
+++ NRCOnboarding/OnboardingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingViewController.swift
+//  NRCOnboarding
+//
+//  Created by bibiga on 1/26/24.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
