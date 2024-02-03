bae04629f548f2b87229f45ee4c2e7702493cca3
Kim ju hwan <minecraft_bos@naver.com>
2024-02-03 17:20:41 +0900

    Timer_2!

--- Time/Time/ContentView.swift
+++ Time/Time/ContentView.swift
@@ -10,12 +10,20 @@ import SwiftUI
 struct ContentView: View {
     let date = Date()
     @State var timeRemaining: Int = 100
+    
     var body: some View {
         ZStack {
-            Text(String(timeRemaining))
+            Text(convertSecondsToTime(timeInSeconds:timeRemaining))
                 .font(.system(size: 50))
         }
     }
+    
+    func convertSecondsToTime(timeInSeconds: Int) -> String {
+            let hours = timeInSeconds / 3600
+            let minutes = (timeInSeconds - hours*3600) / 60
+            let seconds = timeInSeconds % 60
+            return String(format: "%02i:%02i:%02i", hours,minutes,seconds)
+        }
 }
 
 #Preview {
