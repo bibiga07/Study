cc9383824d2a8133b92bf29af74368623b2606da
Kim ju hwan <minecraft_bos@naver.com>
2024-02-13 12:29:21 +0900

    dart_for,while,do-while

--- dart_programing3.dart
+++ dart_programing3.dart
@@ -47,8 +47,60 @@ void main() {
       print("Answer");
     /// ONly Upper Dart 3.0
     case > 100:
-      print("number가 100 초과이다.")
+      print("number가 100 초과이다.");
     default:
       print("Not 1 ~ 5");
   }
+
+  /// 반복문 : 특정한 코드의 반복을 컴퓨터에게 지시 할 때 사용하는 프로그래밍 문법
+  /// for / for in / while / do - while
+  /// continue / break
+  /// 
+  /// for (기준 변수; 조건식; 가변치) {
+  ///   조건식이 참 일 때 반복할 코드
+  /// }
+  for (int i = 0; i < 10; i++) {
+    print("Running For Index $i");
+  }
+  
+  /// for (단일 변수 in 컬렉션 (List / Set / Map)) {
+  ///   컬랙션 내에 요소들의 수 / 변수 만큼 사용 될 반복문
+  /// }
+  
+  List<int> indexs = [0, 1, 2, 3, 4, 5];
+  for (final index in indexs) {
+    print("Running For Index $index");
+  }
+
+  /// while (조건식) {
+  ///   조건식이 참일 경우 실행 될 반복문
+  /// }
+  
+  bool isRunning = true;
+  int count = 0;
+
+  while (true) {
+    if (count >= 5) {
+      break;
+    }
+    count++;
+    print("While is Running");
+  }
+
+  /// do - while
+  /// do {
+  ///   선행 진행 될 코드
+  /// } while (조건);
+  
+  int num = 0;
+
+  do {
+    num++;
+    
+    if (num == 4) {
+      continue;
+    }
+
+    print("Running Do while $num");
+  } while (num < 10);
 }
