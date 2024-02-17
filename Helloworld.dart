db8614350621ced539a0ba1070addee3bdd29dab
Kim ju hwan <minecraft_bos@naver.com>
2024-02-17 23:22:49 +0900

    HelloWorld

--- Helloworld.dart
+++ Helloworld.dart
@@ -1,4 +1,8 @@
 void main() {
   print("Hello"+" World!");
   print("Hello World!");
+  List<String> HW = ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d", "!"];
+  for (final index in HW) {
+    print("$index");
+  }
 }
