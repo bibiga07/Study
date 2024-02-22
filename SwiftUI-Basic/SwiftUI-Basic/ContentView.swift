4fae90e58d51b54f7bad1f63142218b7f1048bd9
Kim ju hwan <minecraft_bos@naver.com>
2024-02-22 23:20:30 +0900

    SwiftUI-Basic3

--- SwiftUI-Basic/SwiftUI-Basic/ContentView.swift
+++ SwiftUI-Basic/SwiftUI-Basic/ContentView.swift
@@ -10,12 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ImageView()
+            ButtonView()
+            TextView()
+            
+            HStack {
+                ImageView()
+                ButtonView()
+                TextView()
+            }
+            
+            ZStack {
+                ImageView()
+                ButtonView()
+                TextView()
+            }
         }
-        .padding()
     }
 }
 
